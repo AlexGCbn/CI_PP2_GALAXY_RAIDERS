bc131dce92e28e775694c16ca2acef6b3f9a749f
Alex Ciobanu <tsopi10@gmail.com>
2021-09-19 17:57:09 +0300

    Add corrected function to create grid and new positioning function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ document.getElementById("start-button").addEventListener("click", createGrids)
 const aliens = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
 ]
+const currentPosition = 21
 
 /**
  * Creates the grids in the HTML file.
@@ -13,7 +14,16 @@ const aliens = [
 function createGrids() {
     for (let x = 0; x < 400; x++) {
         let gridCell = document.createElement("div")
+        gridCell.classList.add("empty-cell")
         document.getElementById("game-area").appendChild(gridCell)
+    }
+    positionAliens()
+}
+
+function positionAliens() {
+    let gridCell = document.getElementsByClassName("empty-cell")
+    for (let alien of aliens) {
+        gridCell[currentPosition - 1 + alien].classList.add("alien")
     }
 }
 
