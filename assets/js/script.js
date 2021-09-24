5953dc1410f250702423a4dce3a90932504161ad
Alex Ciobanu <tsopi10@gmail.com>
2021-09-24 13:25:21 +0300

    Add missing semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,120 +1,120 @@
-document.getElementById("start-button").addEventListener("click", createGrids)
-document.getElementById("right-button").addEventListener("click", moveShipRight)
-document.getElementById("left-button").addEventListener("click", moveShipLeft)
-document.getElementById("initiate-button").addEventListener("click", mainMovement)
-document.getElementById("shoot-button").addEventListener("click", shootRocket)
-window.addEventListener("keydown", gameButtons)
-window.addEventListener("keyup", clearMovementInterval)
+document.getElementById("start-button").addEventListener("click", createGrids);
+document.getElementById("right-button").addEventListener("click", moveShipRight);
+document.getElementById("left-button").addEventListener("click", moveShipLeft);
+document.getElementById("initiate-button").addEventListener("click", mainMovement);
+document.getElementById("shoot-button").addEventListener("click", shootRocket);
+window.addEventListener("keydown", gameButtons);
+window.addEventListener("keyup", clearMovementInterval);
 
 // Set Aliens global variable
 var aliens = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
-]
+];
 
 // Set current position global variable
-var currentPosition = 21
+var currentPosition = 21;
 
-const unusedCells = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-    16, 17, 18, 19, 20, 380, 399]
+var unusedCells = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+    16, 17, 18, 19, 20, 380, 399];
 
 // Interval ID to be able to clear the movement interval in future functions
 var intervalID = null;
 
 // Global variables needed for functions to communicate
-var movement = "right"
-var movesLeft = 0
-var rightMoves = 0
-var leftMoves = 0
-var oppositeExtraMoves = 0
+var movement = "right";
+var movesLeft = 0;
+var rightMoves = 0;
+var leftMoves = 0;
+var oppositeExtraMoves = 0;
 
 // Global variable for ship position
-var shipPosition = 390
+var shipPosition = 390;
 
 // Global variable to control rocket fire
-var rocketCanFire = true
-var waitingForInterval = false
+var rocketCanFire = true;
+var waitingForInterval = false;
 
 // Global interval IDs for keyboard buttons
-var moveRightInterval = null
-var shipMovingRight = false
-var moveLeftInterval = null
-var shipMovingLeft = false
-var shipShootingInterval = null
-var shipShooting = false
+var moveRightInterval = null;
+var shipMovingRight = false;
+var moveLeftInterval = null;
+var shipMovingLeft = false;
+var shipShootingInterval = null;
+var shipShooting = false;
 
 // Global score variable
-var score = 0
-var difficultyScore = 0
+var score = 0;
+var difficultyScore = 0;
 
 /**
  * Removes everything from the grid and replaces it with the victory banner.
  */
 function victory() {
-    document.getElementById("game-area").innerHTML = `<h2 id="victory-banner">VICTORY!</h2>`
-    clearInterval(moveLeftInterval)
-    clearInterval(moveRightInterval)
-    clearInterval(shipShootingInterval)
-    return
+    document.getElementById("game-area").innerHTML = `<h2 id="victory-banner">VICTORY!</h2>`;
+    clearInterval(moveLeftInterval);
+    clearInterval(moveRightInterval);
+    clearInterval(shipShootingInterval);
+    return;
 }
 
 /**
  * Removes everything from the grid and replaces it with the game over banner.
  */
 function gameOver() {
-    document.getElementById("game-area").innerHTML = `<h2 id="game-over-banner">GAME OVER</h2>`
-    clearInterval(moveLeftInterval)
-    clearInterval(moveRightInterval)
-    clearInterval(shipShootingInterval)
-    return
+    document.getElementById("game-area").innerHTML = `<h2 id="game-over-banner">GAME OVER</h2>`;
+    clearInterval(moveLeftInterval);
+    clearInterval(moveRightInterval);
+    clearInterval(shipShootingInterval);
+    return;
 }
 
 /**
  * Calculates if there are destroyed columns in aliens array to the right, so it can add more moves.
  */
 function rightMovesAllRows() {
-    let aliensRow1 = aliens.slice(0, 12)
-    let aliensReverse1 = aliensRow1.slice().reverse()
-    let aliensRow2 = aliens.slice(12, 24)
-    let aliensReverse2 = aliensRow2.slice().reverse()
-    let aliensRow3 = aliens.slice(24, 36)
-    let aliensReverse3 = aliensRow3.slice().reverse()
-    rightMoves = 0
-    console.log("RIGHT moves are: " + rightMoves)
+    let aliensRow1 = aliens.slice(0, 12);
+    let aliensReverse1 = aliensRow1.slice().reverse();
+    let aliensRow2 = aliens.slice(12, 24);
+    let aliensReverse2 = aliensRow2.slice().reverse();
+    let aliensRow3 = aliens.slice(24, 36);
+    let aliensReverse3 = aliensRow3.slice().reverse();
+    rightMoves = 0;
+    console.log("RIGHT moves are: " + rightMoves);
 
     for (let i = 0; i < 12; i++) {
         if (aliensReverse1[i] === 0 && aliensReverse2[i] === 0 && aliensReverse3[i] === 0) {
-            rightMoves++
-            console.log("Add 1 to RIGHT moves. Total: " + rightMoves)
+            rightMoves++;
+            console.log("Add 1 to RIGHT moves. Total: " + rightMoves);
         } else {
-            break
+            break;
         }
     }
 
-    return rightMoves + 6 + leftMoves
+    return rightMoves + 6 + leftMoves;
 }
 
 /**
  * Calculates if there are destroyed columns in aliens array to the right, so it can add more moves.
  */
 function leftMovesAllRows() {
-    let aliensRow1 = aliens.slice(0, 12)
-    let aliensRow2 = aliens.slice(12, 24)
-    let aliensRow3 = aliens.slice(24, 36)
-    leftMoves = 0
-    console.log("LEFT moves are: " + leftMoves)
+    let aliensRow1 = aliens.slice(0, 12);
+    let aliensRow2 = aliens.slice(12, 24);
+    let aliensRow3 = aliens.slice(24, 36);
+    leftMoves = 0;
+    console.log("LEFT moves are: " + leftMoves);
 
     for (let i = 0; i < 12; i++) {
         if (aliensRow1[i] === 0 && aliensRow2[i] === 0 && aliensRow3[i] === 0) {
-            leftMoves++
-            console.log("Add 1 to LEFT moves. Total: " + rightMoves)
+            leftMoves++;
+            console.log("Add 1 to LEFT moves. Total: " + rightMoves);
         } else {
-            break
+            break;
         }
     }
 
-    return leftMoves + 6 + rightMoves
+    return leftMoves + 6 + rightMoves;
 }
 
 /**
@@ -123,26 +123,26 @@ function leftMovesAllRows() {
  */
 function createGrids() {
     for (let x = 0; x < 400; x++) {
-        let gridCellCreator = document.createElement("div")
-        gridCellCreator.classList.add("empty-cell")
+        let gridCellCreator = document.createElement("div");
+        gridCellCreator.classList.add("empty-cell");
         if (unusedCells.includes(x)) {
-        gridCellCreator.classList.add("unused-cell")
+        gridCellCreator.classList.add("unused-cell");
         }
-        document.getElementById("game-area").appendChild(gridCellCreator)
+        document.getElementById("game-area").appendChild(gridCellCreator);
     }
 
-    positionAliens()
+    positionAliens();
     
 
     // Position spaceship
-    let gridCell = document.getElementsByClassName("empty-cell")
-    gridCell[shipPosition].classList.add("spaceship")
+    let gridCell = document.getElementsByClassName("empty-cell");
+    gridCell[shipPosition].classList.add("spaceship");
 
     // Start rocket positioning function
-    positionRockets()
+    positionRockets();
 }
 
-var gridCell = document.getElementsByClassName("empty-cell")
+var gridCell = document.getElementsByClassName("empty-cell");
 
 /**
  * Function to position aliens. It takes the current position variable so it can be called any time.
@@ -150,7 +150,7 @@ var gridCell = document.getElementsByClassName("empty-cell")
 function positionAliens() {
     for (let alien of aliens) {
         if (alien > 0) {
-        gridCell[currentPosition - 1 + alien].classList.add("alien")
+        gridCell[currentPosition - 1 + alien].classList.add("alien");
         }
     }
 }
@@ -160,7 +160,7 @@ function positionAliens() {
  */
 function removeAliens() {
     for (let i = 0; i < gridCell.length; i++) {
-        gridCell[i].classList.remove("alien")
+        gridCell[i].classList.remove("alien");
     }
 }
 
@@ -168,8 +168,8 @@ function removeAliens() {
  * Movement starting function.
  */
 function mainMovement() {
-    movesLeft = rightMovesAllRows()
-    intervalID = setInterval(moveRight, (800 - difficultyScore))
+    movesLeft = rightMovesAllRows();
+    intervalID = setInterval(moveRight, (800 - difficultyScore));
 }
 
 /**
@@ -178,13 +178,13 @@ function mainMovement() {
 function switchMovement() {
     // Switch the movement after 1 second, so it seems like it never stopped
     if (movement === "right") {
-        movement = "left"
-        movesLeft = leftMovesAllRows()
-        intervalID = setInterval(moveLeft, (800 - difficultyScore))
+        movement = "left";
+        movesLeft = leftMovesAllRows();
+        intervalID = setInterval(moveLeft, (800 - difficultyScore));
     } else {
-        movement = "right"
-        movesLeft = rightMovesAllRows()
-        intervalID = setInterval(moveRight, (800 - difficultyScore))
+        movement = "right";
+        movesLeft = rightMovesAllRows();
+        intervalID = setInterval(moveRight, (800 - difficultyScore));
     }
 }
 
@@ -193,15 +193,15 @@ function switchMovement() {
  * Calls functions to remove all aliens then place them again.
  */
 function moveRight() {
-    console.log("Moving right " + movesLeft + " times")
+    console.log("Moving right " + movesLeft + " times");
     if (movesLeft > 0) {
-        currentPosition++
-        removeAliens()
-        positionAliens()
-        movesLeft -= 1
+        currentPosition++;
+        removeAliens();
+        positionAliens();
+        movesLeft -= 1;
     } else {
-        clearInterval(intervalID)
-        moveDown()
+        clearInterval(intervalID);
+        moveDown();
     }
 }
 
@@ -212,13 +212,13 @@ function moveRight() {
 function moveLeft() {
     console.log("Moving left " + movesLeft + " times")
     if (movesLeft > 0) {
-        currentPosition -= 1
-        removeAliens()
-        positionAliens()
-        movesLeft -= 1
+        currentPosition -= 1;
+        removeAliens();
+        positionAliens();
+        movesLeft -= 1;
     } else {
-        clearInterval(intervalID)
-        moveDown()
+        clearInterval(intervalID);
+        moveDown();
     }
 }
 
@@ -231,13 +231,13 @@ function moveLeft() {
 function moveDown() {
     for (let x = 360; x < 380; x++) {
         if (gridCell[x].classList.contains("alien")) {
-            gameOver()
+            gameOver();
         }
     }
-    currentPosition += 20
-    removeAliens()
-    positionAliens()
-    switchMovement()
+    currentPosition += 20;
+    removeAliens();
+    positionAliens();
+    switchMovement();
 }
 
 
@@ -248,9 +248,9 @@ function moveDown() {
 function moveShipRight() {
 
     if (!gridCell[shipPosition + 1].classList.contains("unused-cell")) {
-        gridCell[shipPosition + 1].classList.add("spaceship")
-        gridCell[shipPosition].classList.remove("spaceship")
-        shipPosition++
+        gridCell[shipPosition + 1].classList.add("spaceship");
+        gridCell[shipPosition].classList.remove("spaceship");
+        shipPosition++;
     }
 }
 
@@ -261,9 +261,9 @@ function moveShipRight() {
 function moveShipLeft() {
 
     if (!gridCell[shipPosition - 1].classList.contains("unused-cell")) {
-        gridCell[shipPosition - 1].classList.add("spaceship")
-        gridCell[shipPosition].classList.remove("spaceship")
-        shipPosition--
+        gridCell[shipPosition - 1].classList.add("spaceship");
+        gridCell[shipPosition].classList.remove("spaceship");
+        shipPosition--;
     }
 }
 
@@ -272,10 +272,10 @@ function moveShipLeft() {
  */
 function shootRocket() {
     if (rocketCanFire === true) {
-        gridCell[shipPosition - 20].classList.add("rocket")
-        rocketCanFire = false
-        waitingForInterval = false
-        rocketTimer()
+        gridCell[shipPosition - 20].classList.add("rocket");
+        rocketCanFire = false;
+        waitingForInterval = false;
+        rocketTimer();
     } 
 }
 
@@ -285,8 +285,8 @@ function shootRocket() {
 function rocketTimer() {
     if (!rocketCanFire) {
         setTimeout(() => {
-            rocketCanFire = true
-            waitingForInterval = true
+            rocketCanFire = true;
+            waitingForInterval = true;
         }, 1500); //Controls how frequently rockets can be fired
     }
 }
@@ -295,20 +295,20 @@ function rocketTimer() {
  * Rocket positioning function. Works in tandem with moveRocket()
  */
 function positionRockets() {
-    let rocketCell = document.getElementsByClassName("rocket")
-    let indexNum = 0
-    let rocketIndex = []
+    let rocketCell = document.getElementsByClassName("rocket");
+    let indexNum = 0;
+    let rocketIndex = [];
 
     // Calculate as many rockets as needed
     for (let i = 0; i < gridCell.length; i++) {
         for (let x = 0; x < rocketCell.length; x++) {
             if (gridCell[i] == rocketCell[x]) {
-                rocketIndex.push(indexNum)
+                rocketIndex.push(indexNum);
             }
         }
-        indexNum++
+        indexNum++;
     }
-    setTimeout(moveRocket(rocketIndex), 250)
+    setTimeout(moveRocket(rocketIndex), 250);
 }
 
 /**
@@ -319,18 +319,18 @@ function moveRocket(rockets) {
     for (let i = 0; i < rockets.length; i++) {
         cellNum = rockets[i]
         if (gridCell[cellNum].classList.contains("alien")) {
-            cellNum = cellNum + 20
-            explodeAlien(cellNum)
+            cellNum = cellNum + 20;
+            explodeAlien(cellNum);
         } else if (gridCell[cellNum - 20].classList.contains("alien")) {
-            explodeAlien(cellNum)
+            explodeAlien(cellNum);
         } else if (gridCell[cellNum - 20].classList.contains("unused-cell")) {
-            gridCell[cellNum].classList.remove("rocket")
+            gridCell[cellNum].classList.remove("rocket");
         } else if (gridCell[cellNum - 20].classList.contains("empty-cell")) {
-            gridCell[cellNum].classList.remove("rocket")
-            gridCell[cellNum - 20].classList.add("rocket")
+            gridCell[cellNum].classList.remove("rocket");
+            gridCell[cellNum - 20].classList.add("rocket");
         }
     }
-    setTimeout(positionRockets, 250) // Change timeout for rocket speed
+    setTimeout(positionRockets, 250); // Change timeout for rocket speed
 }
 
 /**
@@ -340,17 +340,17 @@ function moveRocket(rockets) {
  */
 function gameButtons(e) {
     if (e.key === "ArrowRight" && !shipMovingRight && !shipMovingLeft) {
-        setTimeout(moveShipRight, 10)
-        moveRightInterval = setInterval(moveShipRight, 100)
-        shipMovingRight = true
+        setTimeout(moveShipRight, 10);
+        moveRightInterval = setInterval(moveShipRight, 100);
+        shipMovingRight = true;
     } else if (e.key === "ArrowLeft" && !shipMovingLeft && !shipMovingRight) {
-        setTimeout(moveShipLeft, 10)
-        moveLeftInterval = setInterval(moveShipLeft, 100)
-        shipMovingLeft = true
+        setTimeout(moveShipLeft, 10);
+        moveLeftInterval = setInterval(moveShipLeft, 100);
+        shipMovingLeft = true;
     } else if (e.key === "Control" && !shipShooting) {
-        shootRocket()
-        shipShootingInterval = setInterval(shootRocket, 1)
-        shipShooting = true
+        shootRocket();
+        shipShootingInterval = setInterval(shootRocket, 1);
+        shipShooting = true;
     }
 }
 
@@ -359,14 +359,14 @@ function gameButtons(e) {
  */
 function clearMovementInterval(e) {
     if (e.key === "ArrowRight") {
-        shipMovingRight = false
+        shipMovingRight = false;
         clearInterval(moveRightInterval)
     } else if (e.key === "ArrowLeft") {
-        shipMovingLeft = false
+        shipMovingLeft = false;
         clearInterval(moveLeftInterval)
     } else if (e.key === "Control") {
-        shipShooting = false
-        clearInterval(shipShootingInterval)
+        shipShooting = false;
+        clearInterval(shipShootingInterval);
     }
 }
 
@@ -375,26 +375,26 @@ function clearMovementInterval(e) {
  * Adds the "boom" class for 100ms, to give the effect of an explosion.
  */
 function explodeAlien(cellNum) {
-    let arrayIndex = aliens.indexOf(cellNum - 19 - currentPosition)
-    gridCell[cellNum].classList.remove("rocket")
-    gridCell[cellNum + 20].classList.remove("rocket")
-    gridCell[cellNum - 20].classList.remove("rocket")
-    gridCell[cellNum - 20].classList.remove("alien")
-    gridCell[cellNum - 20].classList.add("boom")
-    aliens[arrayIndex] = 0
-    scoreIncrease()
-    difficultyScore = score * 10
-    setTimeout(() => {gridCell[cellNum - 20].classList.remove("boom")}, 50)
-    console.log("BOOM ON CELL " + cellNum)
+    let arrayIndex = aliens.indexOf(cellNum - 19 - currentPosition);
+    gridCell[cellNum].classList.remove("rocket");
+    gridCell[cellNum + 20].classList.remove("rocket");
+    gridCell[cellNum - 20].classList.remove("rocket");
+    gridCell[cellNum - 20].classList.remove("alien");
+    gridCell[cellNum - 20].classList.add("boom");
+    aliens[arrayIndex] = 0;
+    scoreIncrease();
+    difficultyScore = score * 10;
+    setTimeout(() => {gridCell[cellNum - 20].classList.remove("boom")}, 50);
+    console.log("BOOM ON CELL " + cellNum);
 }
 
 /**
  * Increases score and checks for victory or defeat.
  */
 function scoreIncrease() {
-    score++
-    document.getElementById("score").innerHTML = `${score}`
+    score++;
+    document.getElementById("score").innerHTML = `${score}`;
     if (score === 36) {
-        victory()
+        victory();
     }
 }
