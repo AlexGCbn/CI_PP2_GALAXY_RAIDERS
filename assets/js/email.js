abd0c404488c56fa7505ff20711465892d8f1b0a
Alex Ciobanu <tsopi10@gmail.com>
2021-09-29 19:38:32 +0300

    Add emailJS API, create basic email functions

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,9 +1,22 @@
+const form = document.getElementById("feedback-form")
+const fullName = document.getElementById("fname");
+const email = document.getElementById("email");
+const feedbackMessage = document.getElementById("feedback");
+
+form.addEventListener("submit", emailSubmit())
+
+(function() {
+    emailjs.init("user_L7uacW24qxLqTCAOKYO1W");
+})();
+
+function emailSubmit() {
+    e.preventDefault;
+
+    // let fName = fullName.value;
+    // let userEmail = email.value;
+    // let userMessage = feedbackMessage.value;
 
 
-var name = document.getElementById("fname");
-var email = document.getElementById("email");
-var feedbackMessage = document.getElementById("feedback");
 
-function email() {
-
+    emailjs.sendForm("service_tghuhj4", "template_vzaxweo", "#feedback-form", "user_L7uacW24qxLqTCAOKYO1W").then(() { alert("Thank you for your feedback!") });
 }
