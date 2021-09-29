b75b64cb213faf5103a4b363e9c4876215cba5e1
Alex Ciobanu <tsopi10@gmail.com>
2021-09-29 19:01:18 +0300

    Create email JS file / add basic variables / connect it to contact page

---
+++ assets/js/email.js
@@ -0,0 +1,9 @@
+
+
+var name = document.getElementById("fname");
+var email = document.getElementById("email");
+var feedbackMessage = document.getElementById("feedback");
+
+function email() {
+
+}
