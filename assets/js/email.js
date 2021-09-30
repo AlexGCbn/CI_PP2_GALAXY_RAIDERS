0cf5c831947943f82537310467a6b958eddc5bc1
Alex Ciobanu <tsopi10@gmail.com>
2021-09-30 14:19:14 +0300

    Add functionality to emailJS API and test if it works

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,20 +3,13 @@ const fullName = document.getElementById("fname");
 const email = document.getElementById("email");
 const feedbackMessage = document.getElementById("feedback");
 
-form.addEventListener("submit", emailSubmit())
+document.getElementById("feedback-form").addEventListener("submit", emailSubmit)
 
-(function() {
-    emailjs.init("user_L7uacW24qxLqTCAOKYO1W");
-})();
+function emailSubmit(event) {
+    event.preventDefault();
+    emailjs.sendForm("service_tghuhj4", "template_vzaxweo", form)
+        .then(() => { alert("Thank you for your feedback!") },
+        (error) => { console.log("Failed")});
 
-function emailSubmit() {
-    e.preventDefault;
-
-    // let fName = fullName.value;
-    // let userEmail = email.value;
-    // let userMessage = feedbackMessage.value;
-
-
-
-    emailjs.sendForm("service_tghuhj4", "template_vzaxweo", "#feedback-form", "user_L7uacW24qxLqTCAOKYO1W").then(() { alert("Thank you for your feedback!") });
-}
+    
+};
