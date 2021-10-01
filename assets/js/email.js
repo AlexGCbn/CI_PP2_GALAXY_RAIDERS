4794cc05d3e8c4d385c64f78c047cc6e2dfeba33
Alex Ciobanu <tsopi10@gmail.com>
2021-10-01 14:45:24 +0300

    Change validation counter for email sending condition

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,21 +4,30 @@ const formEmail = document.getElementById("email");
 const feedbackMessage = document.getElementById("feedback");
 const errorClasses = document.getElementsByClassName("error-message");
 
+var okCounter = 0;
+
 document.getElementById("feedback-form").addEventListener("submit", validationStart);
 
 function emailSubmit() {
     // event.preventDefault();
+    console.log("Sending email!");
     emailjs.sendForm("service_tghuhj4", "template_vzaxweo", form)
-        .then(() => { alert("Thank you for your feedback!") },
-        (error) => { console.log("Failed")});
-};
+        .then(() => { alert("Thank you for your feedback!"); },
+        (error) => { console.log("Failed");});
+}
 
 function validationStart(event) {
     event.preventDefault();
 
+    okCounter = 0;
+    console.log("Pressed submit");
     validation(fullName, 0, "Name cannot be empty.");
     validation(formEmail, 1, "Email cannot be empty.");
-    validation(feedbackMessage, 2, "Feedback field cannot be empty.")
+    validation(feedbackMessage, 2, "Feedback field cannot be empty.");
+    if (okCounter === 3) {
+        console.log("errorClasses");
+        emailSubmit();
+    }
 }
 
 function validation(field, classNr, message) {
@@ -28,8 +37,7 @@ function validation(field, classNr, message) {
     } else {
         errorClasses[classNr].innerHTML = "";
         field.style.border = "2px solid green";
-    }
-    if (errorClasses[1] === errorClasses[2] === errorClasses[3]) {
-        emailSubmit()
+        console.log("Validated");
+        okCounter++;
     }
 }
